f2f2127a5b6b2bae8b47e7297187d97014d27762
Charbel <charbel.dawoud@gmail.com>
2024-12-17 17:59:45 +0100

    JS <> Implemented the random function + Blinking

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+const h2 = document.querySelector("h2");
+const red = document.getElementById("red");
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+const yellow = document.getElementById("yellow");
+const colors = [red, green, blue, yellow]; 
+
+let current_level = 0; 
+let game_pattern = []; 
+let user_pattern = []; 
+
+function randomElement(){
+    return colors[Math.floor(Math.random()*colors.length)];
+}
+ function blink(element){
+    const backgroundColor = getComputedStyle(element).backgroundColor; 
+    element.style.boxShadow = "0px 0px 5px 10px grey";
+    element.style.backgroundColor = "grey";
+    setTimeout(() =>{
+        element.style.backgroundColor = `${backgroundColor}`;
+        element.style.boxShadow = "0px 0px 0px 0px";
+    }, 100);
+    
+ }
+
+ blink(randomElement());
